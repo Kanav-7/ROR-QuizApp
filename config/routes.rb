ead999fbb288d65aac79f37a5a3584497e9f08ac
Kanav Gupta <kanav.gupta@students.iiit.ac.in>
2017-09-15 23:32:50 +0530

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # devise_for :users
+  devise_for :users
+  # , :controllers => { :omniauth_callbacks => "callbacks" }
+  get 'users/:id' => 'users#profile'
+  get 'pages/index'
+  root to:'pages#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
